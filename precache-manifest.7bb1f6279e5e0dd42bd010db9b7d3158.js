b5af965e9e2ccf21335d4fa03370d06545251e65
Vadluri <sai_vadluri@optum.com>
2020-08-21 13:13:00 +0530

    Updates

---
+++ precache-manifest.7bb1f6279e5e0dd42bd010db9b7d3158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b1edee85dd7c4db68df8a71fd6639d",
+    "url": "/Covid-Dashboard/index.html"
+  },
+  {
+    "revision": "be68a60b4239bdb39f3f",
+    "url": "/Covid-Dashboard/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f5ff620ff4f140d7f0e0",
+    "url": "/Covid-Dashboard/static/css/main.a8960c8d.chunk.css"
+  },
+  {
+    "revision": "be68a60b4239bdb39f3f",
+    "url": "/Covid-Dashboard/static/js/2.90af5f23.chunk.js"
+  },
+  {
+    "revision": "92d6d832963b73fc9251f6623e89f44b",
+    "url": "/Covid-Dashboard/static/js/2.90af5f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ff620ff4f140d7f0e0",
+    "url": "/Covid-Dashboard/static/js/main.6e567c23.chunk.js"
+  },
+  {
+    "revision": "88d3261c800c1a851eaa",
+    "url": "/Covid-Dashboard/static/js/runtime-main.153bcf4b.js"
+  }
+]);
